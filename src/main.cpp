f5e0b919846d657962b62fcda754170115579b63
Tyler <tylergross28@gmail.com>
2024-01-01 12:51:35 -0500

    better types for pointers

--- src/main.cpp
+++ src/main.cpp
@@ -20,25 +20,25 @@ auto to_lower_case(std::string str) {
 }
 
 template <typename F>
-F create_function(size_t ptr) {
+F create_function(intptr_t ptr) {
   return reinterpret_cast<F>(ptr);
 }
 
-std::vector<std::string>* create_vector_from_ptr(size_t vec) {
-  return reinterpret_cast<std::vector<std::string>*>(vec);
+std::vector<std::string>* create_vector_from_ptr(intptr_t ptr) {
+  return reinterpret_cast<std::vector<std::string>*>(ptr);
 }
 
 template <typename K, typename V>
 auto get_keys(std::map<K, V> map) {
   const std::vector<K> keys = {};
-  for (const auto& [key] : map) {
+  for (const auto& [key, _value] : map) {
     keys.push_back(key);
   }
 
   return keys;
 }
 
-auto get_buffer(size_t buffer_ptr, size_t buffer_size) {
+auto get_buffer(intptr_t buffer_ptr, intptr_t buffer_size) {
   const auto ptr = reinterpret_cast<uint8_t*>(buffer_ptr);
   return emscripten::val(emscripten::typed_memory_view<uint8_t>(buffer_size, ptr));
 }
@@ -49,7 +49,7 @@ auto output_error(archive* active_archive, int return_code) {
   }
 }
 
-auto get_archive(size_t archive_file_ptr, size_t archive_file_size) {
+auto get_archive(intptr_t archive_file_ptr, intptr_t archive_file_size) {
   auto return_code = ARCHIVE_OK;
   const auto read_archive = archive_read_new();
 
@@ -85,7 +85,7 @@ auto for_each_entry(archive* read_archive, std::function<void(archive_entry*, st
   }
 }
 
-auto list_entry_paths(size_t archive_file_ptr, size_t archive_file_size) {
+auto list_entry_paths(intptr_t archive_file_ptr, intptr_t archive_file_size) {
   auto return_code = ARCHIVE_OK;
   archive_entry* entry = nullptr;
 
@@ -101,7 +101,7 @@ auto list_entry_paths(size_t archive_file_ptr, size_t archive_file_size) {
 
 struct buffer_object {
   void* buffer;
-  size_t ptr;
+  intptr_t ptr;
   size_t size;
   std::function<void()> free;
 };
@@ -111,19 +111,20 @@ auto read_entry(archive* read_archive, archive_entry* entry) {
   void* read_buffer = malloc(size);
 
   archive_read_data(read_archive, read_buffer, size);
-  const auto ptr = reinterpret_cast<size_t>(read_buffer);
+  const auto ptr = reinterpret_cast<intptr_t>(read_buffer);
   return (struct buffer_object){.ptr = ptr, .size = size, .free = [&] { free(read_buffer); }};
 }
 
-auto extract_single_entry(size_t archive_file_ptr,
-                          size_t archive_file_size,
+auto extract_single_entry(intptr_t archive_file_ptr,
+                          intptr_t archive_file_size,
                           std::string entry_name,
-                          size_t on_found_callback_ptr) {
+                          intptr_t on_found_callback_ptr) {
   auto return_code = ARCHIVE_OK;
   const auto read_archive = get_archive(archive_file_ptr, archive_file_size);
 
   auto called = false;
-  const auto on_found = reinterpret_cast<void (*)(const char* path, size_t ptr, size_t size)>(on_found_callback_ptr);
+  const auto on_found =
+      reinterpret_cast<void (*)(const char* path, intptr_t ptr, intptr_t size)>(on_found_callback_ptr);
   for_each_entry(read_archive, [&](const auto entry, const auto entry_path) {
     if (entry_path == entry_name) {
       const auto data = read_entry(read_archive, entry);
@@ -141,11 +142,11 @@ auto extract_single_entry(size_t archive_file_ptr,
   output_error(read_archive, return_code);
 }
 
-auto extract_all_entries(size_t archive_file_ptr, size_t archive_file_size, size_t on_read_callback_ptr) {
+auto extract_all_entries(intptr_t archive_file_ptr, intptr_t archive_file_size, intptr_t on_read_callback_ptr) {
   auto return_code = ARCHIVE_OK;
   const auto read_archive = get_archive(archive_file_ptr, archive_file_size);
 
-  const auto on_read = reinterpret_cast<void (*)(const char* path, size_t ptr, size_t size)>(on_read_callback_ptr);
+  const auto on_read = reinterpret_cast<void (*)(const char* path, intptr_t ptr, intptr_t size)>(on_read_callback_ptr);
   for_each_entry(read_archive, [&](const auto entry, const auto entry_path) {
     const auto data = read_entry(read_archive, entry);
     on_read(entry_path.c_str(), data.ptr, data.size);
