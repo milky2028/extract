52f2d4817d6591a136bc2c6b0e66c1d7e68373d9
Tyler <tylergross28@gmail.com>
2024-01-02 10:07:47 -0500

    check if entry is a file

--- src/main.cpp
+++ src/main.cpp
@@ -83,6 +83,10 @@ size_t get_entry_size(intptr_t entry_ptr) {
   return archive_entry_size(int_to_ptr<archive_entry*>(entry_ptr));
 }
 
+auto entry_is_file(intptr_t entry_ptr) {
+  return archive_entry_filetype(int_to_ptr<archive_entry*>(entry_ptr)) == 3276;
+}
+
 auto read_entry_data(intptr_t archive_ptr, intptr_t entry_ptr) {
   const auto entry = int_to_ptr<archive_entry*>(entry_ptr);
   const size_t size = archive_entry_size(entry);
@@ -98,15 +102,18 @@ auto free_buffer(intptr_t buffer_ptr) {
 
 EMSCRIPTEN_BINDINGS(module) {
   emscripten::function("get_buffer", &get_buffer, emscripten::allow_raw_pointers());
+  emscripten::function("free_buffer", &free_buffer, emscripten::allow_raw_pointers());
+
   emscripten::constant("END_OF_FILE", END_OF_FILE);
   emscripten::constant("ENTRY_ERROR", ENTRY_ERROR);
 
   emscripten::function("open_archive", &open_archive, emscripten::allow_raw_pointers());
   emscripten::function("close_archive", &close_archive, emscripten::allow_raw_pointers());
+  emscripten::function("skip_extraction", &skip_extraction, emscripten::allow_raw_pointers());
+
   emscripten::function("get_next_entry", &get_next_entry, emscripten::allow_raw_pointers());
-  emscripten::function("read_entry_data", &read_entry_data, emscripten::allow_raw_pointers());
   emscripten::function("get_entry_size", &get_entry_size, emscripten::allow_raw_pointers());
   emscripten::function("get_entry_name", &get_entry_name, emscripten::allow_raw_pointers());
-  emscripten::function("free_buffer", &free_buffer, emscripten::allow_raw_pointers());
-  emscripten::function("skip_extraction", &skip_extraction, emscripten::allow_raw_pointers());
+  emscripten::function("read_entry_data", &read_entry_data, emscripten::allow_raw_pointers());
+  emscripten::function("entry_is_file", &entry_is_file, emscripten::allow_raw_pointers());
 }
