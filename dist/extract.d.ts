8fdd0c10abff811a79372f8db97bbeca6adc3cfa
Tyler <tylergross28@gmail.com>
2024-05-31 12:56:42 -0400

    allow larger pointers

--- dist/extract.d.ts
+++ dist/extract.d.ts
@@ -12,9 +12,9 @@ interface EmbindModule {
   close_archive(_0: number): void;
   skip_extraction(_0: number): void;
   get_next_entry(_0: number): number;
+  get_entry_size(_0: number): number;
   read_entry_data(_0: number, _1: number): number;
   entry_is_file(_0: number): boolean;
-  get_entry_size(_0: number): number;
   get_entry_name(_0: number): string;
   get_buffer(_0: number, _1: number): any;
 }
