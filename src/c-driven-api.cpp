10dac825e3b15e5d8decc5c539c8dd5b9edfd641
Tyler Gross <tylergross28@gmail.com>
2025-05-04 15:31:38 -0400

    C API status with broken filesystem

--- src/c-driven-api.cpp
+++ src/c-driven-api.cpp
@@ -2,6 +2,7 @@
 #include <archive_entry.h>
 
 #include <emscripten.h>
+#include <emscripten/val.h>
 #include <emscripten/bind.h>
 #include <emscripten/threading.h>
 #include <emscripten/threading_legacy.h>
@@ -16,26 +17,11 @@
 #include <iostream>
 #include <string>
 #include <thread>
+#include <filesystem>
 
 EMSCRIPTEN_DECLARE_VAL_TYPE(VoidFunction);
 EMSCRIPTEN_DECLARE_VAL_TYPE(OnFailure);
 EMSCRIPTEN_DECLARE_VAL_TYPE(OnEntry);
-
-template <class F>
-void run_async(F&& f) {
-  using function_type = typename std::remove_reference<F>::type;
-  auto p = new function_type(std::forward<F>(f));
-  emscripten_async_run_in_main_runtime_thread(EM_FUNC_SIG_VI, static_cast<void (*)(void*)>([](void* f_) {
-                                                emscripten_async_call(
-                                                    [](void* f_) {
-                                                      auto f = static_cast<function_type*>(f_);
-                                                      (*f)();
-                                                      delete f;
-                                                    },
-                                                    f_, 0);
-                                              }),
-                                              p);
-}
 
 std::string get_entry_name(std::string path) {
   auto path_splitter = "/";
@@ -64,13 +50,19 @@ bool is_image(std::string path) {
   return path.ends_with(".jpg") || path.ends_with(".png");
 }
 
+struct extract_params {
+  std::string archive_source_path;
+  bool extract_data;
+  VoidFunction on_completion = static_cast<VoidFunction>(emscripten::val::global("Function").new_());
+  OnFailure on_failure = static_cast<OnFailure>(emscripten::val::global("Function").new_());
+  OnEntry on_entry = static_cast<OnEntry>(emscripten::val::global("Function").new_());
+};
+
 const int WEB_BLOCK_SIZE = 65536;
-void extract(std::string archive_source_path,
-             bool extract_data,
-             VoidFunction on_completion,
-             OnFailure on_failure,
-             OnEntry on_entry) {
-  std::thread([archive_source_path, extract_data, on_completion, on_failure, on_entry] {
+void extract(extract_params params) {
+    printf("archive path: %s\n", params.archive_source_path.c_str());
+    printf("archive exists? %d", std::filesystem::exists(params.archive_source_path));
+
     auto return_code = ARCHIVE_OK;
     const auto arch = archive_read_new();
 
@@ -80,9 +72,9 @@ void extract(std::string archive_source_path,
     archive_read_support_format_rar(arch);
     archive_read_support_format_zip(arch);
 
-    return_code = archive_read_open_filename(arch, archive_source_path.c_str(), WEB_BLOCK_SIZE);
+    return_code = archive_read_open_filename(arch, params.archive_source_path.c_str(), WEB_BLOCK_SIZE);
     if (return_code < ARCHIVE_OK) {
-      run_async([on_failure, &arch] { on_failure(std::string(archive_error_string(arch))); });
+      params.on_failure(std::string(archive_error_string(arch)));
       return;
     }
 
@@ -91,48 +83,48 @@ void extract(std::string archive_source_path,
       return_code = archive_read_next_header(arch, &entry);
       if (return_code < ARCHIVE_OK) {
         archive_read_free(arch);
-        run_async([on_failure, &arch] { on_failure(std::string(archive_error_string(arch))); });
+        params.on_failure(std::string(archive_error_string(arch)));
         return;
       }
 
       if (return_code == ARCHIVE_EOF) {
         archive_read_free(arch);
-        run_async(on_completion);
+        params.on_completion();
         return;
       }
 
       auto entry_path = archive_entry_pathname(entry);
       if (is_file(entry) && !to_lower_case(entry_path).starts_with("__macosx") && is_image(entry_path)) {
         auto entry_name = get_entry_name(entry_path);
-        if (extract_data) {
+        if (params.extract_data) {
           auto entry_size = archive_entry_size(entry);
           auto entry_data_buffer = static_cast<uint8_t*>(malloc(entry_size));
           archive_read_data(arch, entry_data_buffer, entry_size);
 
-          run_async([on_entry, entry_name, entry_size, &entry_data_buffer] {
-            on_entry(entry_name, emscripten::typed_memory_view<uint8_t>(entry_size, entry_data_buffer));
-          });
+          params.on_entry(entry_name, emscripten::typed_memory_view<uint8_t>(entry_size, entry_data_buffer));
         } else {
-          run_async([on_entry, entry_name] { on_entry(entry_name); });
+          params.on_entry(entry_name, emscripten::val::null());
         }
       }
     }
-  }).detach();
 }
 
-void mount_filesystem(VoidFunction on_complete) {
-  std::thread([on_complete] {
-    auto opfs = wasmfs_create_opfs_backend();
-    wasmfs_create_directory("/opfs", 0777, opfs);
-
-    run_async(on_complete);
-  }).detach();
+bool mount_filesystem() {
+  auto opfs = wasmfs_create_opfs_backend();
+  return wasmfs_create_directory("opfs", 0777, opfs) == 0 ? true : false;
 }
 
 EMSCRIPTEN_BINDINGS(module) {
   emscripten::register_type<VoidFunction>("VoidFunction");
   emscripten::register_type<OnFailure>("(errorMessage: string) => void");
   emscripten::register_type<OnEntry>("(name: string, buffer?: Uint8Array) => void");
+
+  emscripten::value_object<extract_params>("ExtractParams")
+      .field("archive_source_path", &extract_params::archive_source_path)
+      .field("extract_data", &extract_params::extract_data)
+      .field("on_completion", &extract_params::on_completion)
+      .field("on_failure", &extract_params::on_failure)
+      .field("on_entry", &extract_params::on_entry);
 
   emscripten::function("extract", &extract);
   emscripten::function("mount_filesystem", &mount_filesystem);
